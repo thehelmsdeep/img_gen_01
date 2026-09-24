6a3df4c7efb463cac5aaff9f9c654bb2735fa047
Ashkan <107793546+thehelmsdeep@users.noreply.github.com>
2026-09-24 21:50:14 +0330

    Expose manual denoising loop

--- src/generation/__init__.py
+++ src/generation/__init__.py
@@ -1,2 +1,2 @@
-from .latents import create_latents, create_scheduler, add_noise, predict_noise, scheduler_step
+from .latents import create_latents, create_scheduler, add_noise, predict_noise, scheduler_step, denoise
 from .text_to_image import generate
