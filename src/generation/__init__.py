cbb911d93262540fff0eaccc27b527a4a7970d0b
Ashkan <107793546+thehelmsdeep@users.noreply.github.com>
2026-09-24 22:20:14 +0330

    Fix generation package syntax

--- src/generation/__init__.py
+++ src/generation/__init__.py
@@ -1,3 +1,6 @@
-from .latents import create_latents, create_scheduler, add_noise, predict_noise, scheduler_step, denoise
+from .latents import (
+    create_latents,
+    denoise,
+    decode_latents,
+)
 from .text_to_image import generate
-, decode_latents
